b5a2ff95d7789db018fc59aa6e49d862252423b7
mohammad izadi <Mohammad.Izadi@Tapsi.cab>
2024-12-10 21:15:11 +0330

    complete crud endpoints

--- interface/app.js
+++ interface/app.js
@@ -18,12 +18,31 @@ app.get('/:id', function (req, res) {
 app.post('/', function (req, res) {
     fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
         var users = JSON.parse( data );
-        var user = req.body.user4;
+        var user = req.body;
         users["user"+user.id] = user
         res.end( JSON.stringify(users));
     });
 })
 
+app.delete('/:id', function (req, res) {
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+        data = JSON.parse( data );
+        var id = "user"+req.params.id;
+        var user = data[id];
+        delete data[ "user"+req.params.id];
+        res.end( JSON.stringify(data));
+    });
+})
+
+app.put("/:id", function(req, res) {
+    fs.readFile( __dirname + "/" + "users.json", 'utf8', function (err, data) {
+
+        var users = JSON.parse( data );
+        var id = "user"+req.params.id;
+        users[id]=req.body;
+        res.end( JSON.stringify(users));
+    })
+})
 
 var server = app.listen(5000, function () {
     console.log("Express App running at http://127.0.0.1:5000/");
